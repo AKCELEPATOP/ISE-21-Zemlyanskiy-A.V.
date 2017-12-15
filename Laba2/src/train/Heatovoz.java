4cb286915c4707f4f00e1b7b79992127d01490fa
sasha98zem <sasha98zem@gmail.com>
2017-12-15 12:18:09 +0300

    just work shit

--- Laba2/src/train/Heatovoz.java
+++ Laba2/src/train/Heatovoz.java
@@ -8,65 +8,54 @@ public class Heatovoz extends Locomotive {
 
 	private boolean botmChimney;
 
-    private boolean topChimney;
+	private boolean topChimney;
 
-    private int countFuel;
+	private int countFuel;
 
-    private Color chimneyColor;
+	private Color chimneyColor;
 
-    public Heatovoz(int maxCountPassengers,
-                    int maxSpeed,
-                    double carrying,
-                    double weight,
-                    Color colorBody,
-                    Color dopColor,
-                    boolean botmChimney,
-                    boolean topChimney,
-                    int countFuel,
-                    Color chimneyColor) {
-        super(maxCountPassengers,
-                maxSpeed,
-                carrying,
-                weight,
-                colorBody,
-                dopColor);
-        this.botmChimney=botmChimney;
-        this.topChimney=topChimney;
-        this.countFuel=countFuel;
-        this.chimneyColor=chimneyColor;
-    }
-    @Override
-    protected void drawLocomotive(Graphics g){
-        super.drawLocomotive(g);
+	public Heatovoz(int maxCountPassengers, int maxSpeed, double carrying, double weight, Color colorBody,
+			Color dopColor, boolean botmChimney, boolean topChimney, int countFuel, Color chimneyColor) {
+		super(maxCountPassengers, maxSpeed, carrying, weight, colorBody, dopColor);
+		this.botmChimney = botmChimney;
+		this.topChimney = topChimney;
+		this.countFuel = countFuel;
+		this.chimneyColor = chimneyColor;
+	}
 
-        if (botmChimney)
-        {
-            g.setColor(chimneyColor);
-            Rectangle[] botmChimney = new Rectangle[2];
-            botmChimney[0] = new Rectangle((getBodyWidth()>>1) - getBodyWidth() / 3 + 2, (getBodyHeight()>>1) - getBodyHeight() / 12 + 2,
-                    2 * getBodyWidth() / 3 - 4, 2 * getBodyWidth() / 3 - 4);
-            botmChimney[1] = new Rectangle(botmChimney[0].x + 1, botmChimney[0].y + 1, botmChimney[0].width - 2, botmChimney[0].height - 2);
-            g.fillOval(botmChimney[0].x,botmChimney[0].y,botmChimney[0].width,botmChimney[0].height);
-            g.setColor(Color.BLACK);
-            g.fillOval(botmChimney[1].x,botmChimney[1].y,botmChimney[1].width,botmChimney[1].height);
-        }
+	@Override
+	protected void drawLocomotive(Graphics g) {
+		super.drawLocomotive(g);
 
-        if (topChimney)
-        {
-            g.setColor(chimneyColor);
-            Rectangle[] topChimney = new Rectangle[2];
-            topChimney[0] = new Rectangle((getBodyWidth()>>1) - getBodyWidth() / 3 + 2, (getBodyHeight()>>1) - getBodyHeight() / 4 + 2,
-                    2 * getBodyWidth() / 3 - 4, 2 * getBodyWidth() / 3 - 4);
-            topChimney[1] = new Rectangle(topChimney[0].x + 1, topChimney[0].y + 1,
-                    topChimney[0].width - 2, topChimney[0].height - 2);
-            g.fillOval(topChimney[0].x,topChimney[0].y,topChimney[0].width,topChimney[0].height);
-            g.setColor(Color.black);
-            g.fillOval(topChimney[1].x,topChimney[1].y,topChimney[1].width,topChimney[1].height);
-        }
+		if (botmChimney) {
+			g.setColor(chimneyColor);
+			Rectangle[] botmChimney = new Rectangle[2];
+			botmChimney[0] = new Rectangle((getBodyWidth() >> 1) - getBodyWidth() / 3 + 2,
+					(getBodyHeight() >> 1) - getBodyHeight() / 12 + 2, 2 * getBodyWidth() / 3 - 4,
+					2 * getBodyWidth() / 3 - 4);
+			botmChimney[1] = new Rectangle(botmChimney[0].x + 1, botmChimney[0].y + 1, botmChimney[0].width - 2,
+					botmChimney[0].height - 2);
+			g.fillOval(botmChimney[0].x, botmChimney[0].y, botmChimney[0].width, botmChimney[0].height);
+			g.setColor(Color.BLACK);
+			g.fillOval(botmChimney[1].x, botmChimney[1].y, botmChimney[1].width, botmChimney[1].height);
+		}
 
-    }
-    public void setChimneyColor(Color chimneyColor)
-    {
-        this.chimneyColor = chimneyColor;
-    }
+		if (topChimney) {
+			g.setColor(chimneyColor);
+			Rectangle[] topChimney = new Rectangle[2];
+			topChimney[0] = new Rectangle((getBodyWidth() >> 1) - getBodyWidth() / 3 + 2,
+					(getBodyHeight() >> 1) - getBodyHeight() / 4 + 2, 2 * getBodyWidth() / 3 - 4,
+					2 * getBodyWidth() / 3 - 4);
+			topChimney[1] = new Rectangle(topChimney[0].x + 1, topChimney[0].y + 1, topChimney[0].width - 2,
+					topChimney[0].height - 2);
+			g.fillOval(topChimney[0].x, topChimney[0].y, topChimney[0].width, topChimney[0].height);
+			g.setColor(Color.black);
+			g.fillOval(topChimney[1].x, topChimney[1].y, topChimney[1].width, topChimney[1].height);
+		}
+
+	}
+
+	public void setChimneyColor(Color chimneyColor) {
+		this.chimneyColor = chimneyColor;
+	}
 }
--- Laba2/src/train/Locomotive.java
+++ Laba2/src/train/Locomotive.java
@@ -7,119 +7,100 @@ public class Locomotive extends railwayRollingStock {
 
 	private static final long serialVersionUID = -1779687022713597152L;
 
-	public Locomotive(int maxCountPassengers,
-                      int maxSpeed,
-                      double carrying,
-                      double weight,
-                      Color colorBody,
-                      Color dopColor) {
-        super( 	maxCountPassengers,
-                maxSpeed,
-                carrying,
-                weight,
-                colorBody,
-                dopColor);
-    }
-    
-    protected Locomotive(){}
+	public Locomotive(int maxCountPassengers, int maxSpeed, double carrying, double weight, Color colorBody,
+			Color dopColor) {
+		super(maxCountPassengers, maxSpeed, carrying, weight, colorBody, dopColor);
+	}
 
-    @Override
-    public void move(Graphics g) {
-        startPosY -= (getMaxSpeed() * 5 / (int)getWeight()/
-                (countPassengers == 0 ? 1 : countPassengers));
-        draw(g);
-    }
-
-    @Override
-    public void draw(Graphics g) {
-        drawLocomotive(g);
-    }
-
-    protected void drawLocomotive(Graphics g) {
-        int leftSide = 0;
-        int rightSide = getBodyWidth();
-
-        int top = 0;
-        Rectangle body = new Rectangle(leftSide, top,
-                getBodyWidth(), getBodyHeight());
-
-        g.setColor(getColorBody());
-        g.fillRect(leftSide, top,
-                getBodyWidth(), getBodyHeight());
-
-        //Pen pen = new Pen(Color.Black);
-        g.setColor(dopColor);
-
-        g.fillRect(leftSide, top + getBodyHeight() / 6,
-                getBodyWidth(), 5);
-        //решетки
-        g.setColor(Color.BLACK);
-        g.drawLine(leftSide + getBodyWidth() / 12, top + getBodyHeight() / 6 + 5,
-                leftSide + getBodyWidth() / 12, top + getBodyHeight() / 3);
-        g.drawLine(rightSide - getBodyWidth() / 12, top + getBodyHeight() / 6 + 5,
-                rightSide - getBodyWidth() / 12, top + getBodyHeight() / 3);
-        Point[] points = new Point[2];
-        points[0] = new Point(leftSide + getBodyWidth() / 6, top + getBodyHeight() / 3 + getBodyWidth());
-        points[1] = new Point(rightSide - getBodyWidth() / 6, points[0].y);
-
-        for (int i = 0; i < 4; i++)
-        {
-            g.drawLine( points[0].x,points[0].y, points[1].x,points[1].y);
-            points[0].y += 2; points[1].y += 2;
-            g.drawLine(points[0].x,points[0].y, points[1].x,points[1].y);
-            points[0].y += getBodyWidth()/7-2; points[1].y += getBodyWidth() / 7 - 2;
-        }
-
-        //квадратики
-        Rectangle[] rect = new Rectangle[2];
-        rect[0] = new Rectangle(leftSide, top + getBodyHeight() / 3, getBodyWidth() / 6, getBodyWidth() / 6);
-        rect[1] = new Rectangle(rightSide - getBodyWidth() / 6, top + getBodyHeight() / 3,
-                getBodyWidth() / 6, getBodyWidth() / 6);
-
-        for (int i = 0; i < 5; i++)
-        {
-            for (int j = 0; j < 2; j++)
-            {
-                g.setColor(dopColor);
-                g.fillRect(rect[j].x,rect[j].y,rect[j].width,rect[j].height);
-                g.setColor(Color.gray.darker());
-                g.drawRect(rect[j].x,rect[j].y,rect[j].width,rect[j].height);
-                if (i < 4)
-                {
-                    rect[j].y += getBodyWidth() / 3;
-                }
-            }
-        }
-        g.setColor(Color.BLACK);
-
-        //shit
-        g.drawLine(leftSide + getBodyWidth() / 6, top,
-                leftSide + getBodyWidth() / 6, getBodyHeight());
-        g.drawLine( rightSide - getBodyWidth() / 6, top,
-                getBodyWidth() - getBodyWidth() / 6, getBodyHeight());
-        g.drawRect( leftSide, top + getBodyHeight() / 6,
-                getBodyWidth(), 5);
-        g.drawLine( leftSide, top + getBodyHeight() / 3, leftSide + getBodyWidth() / 6, top + getBodyHeight() / 3);
-        g.drawLine( rightSide, top + getBodyHeight() / 3, rightSide - getBodyWidth() / 6, top + getBodyHeight() / 3);
-        g.drawLine( rect[0].x, rect[0].y + rect[0].height, rect[1].x + rect[1].width, rect[0].y + rect[0].height);
-
-        //фары
-        g.fillRect(leftSide + getBodyWidth() / 6, top + getBodyHeight() / 6-5,
-                8, 5);
-        g.fillRect(rightSide - getBodyWidth() / 6-8, top + getBodyHeight() / 6 - 5,
-                8, 5);
-
-        g.drawRect(body.x,body.y,body.width,body.height);
-    }
+	protected Locomotive() {
+	}
 
 	@Override
-    public BufferedImage Pict(){
-    	BufferedImage image=new BufferedImage(getBodyWidth()+1,getBodyHeight()+1,BufferedImage.TYPE_INT_RGB);
-    	Graphics g=image.createGraphics();
-    	g.setColor(Color.WHITE);
-    	g.fillRect(0, 0, image.getWidth(), image.getHeight());
-    	draw(g);
-    	return image;
-    }
-	
+	public void move(Graphics g) {
+		startPosY -= (getMaxSpeed() * 5 / (int) getWeight() / (countPassengers == 0 ? 1 : countPassengers));
+		draw(g);
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		drawLocomotive(g);
+	}
+
+	protected void drawLocomotive(Graphics g) {
+		int leftSide = 0;
+		int rightSide = getBodyWidth();
+
+		int top = 0;
+		Rectangle body = new Rectangle(leftSide, top, getBodyWidth(), getBodyHeight());
+
+		g.setColor(getColorBody());
+		g.fillRect(leftSide, top, getBodyWidth(), getBodyHeight());
+
+		// Pen pen = new Pen(Color.Black);
+		g.setColor(dopColor);
+
+		g.fillRect(leftSide, top + getBodyHeight() / 6, getBodyWidth(), 5);
+		// решетки
+		g.setColor(Color.BLACK);
+		g.drawLine(leftSide + getBodyWidth() / 12, top + getBodyHeight() / 6 + 5, leftSide + getBodyWidth() / 12,
+				top + getBodyHeight() / 3);
+		g.drawLine(rightSide - getBodyWidth() / 12, top + getBodyHeight() / 6 + 5, rightSide - getBodyWidth() / 12,
+				top + getBodyHeight() / 3);
+		Point[] points = new Point[2];
+		points[0] = new Point(leftSide + getBodyWidth() / 6, top + getBodyHeight() / 3 + getBodyWidth());
+		points[1] = new Point(rightSide - getBodyWidth() / 6, points[0].y);
+
+		for (int i = 0; i < 4; i++) {
+			g.drawLine(points[0].x, points[0].y, points[1].x, points[1].y);
+			points[0].y += 2;
+			points[1].y += 2;
+			g.drawLine(points[0].x, points[0].y, points[1].x, points[1].y);
+			points[0].y += getBodyWidth() / 7 - 2;
+			points[1].y += getBodyWidth() / 7 - 2;
+		}
+
+		// квадратики
+		Rectangle[] rect = new Rectangle[2];
+		rect[0] = new Rectangle(leftSide, top + getBodyHeight() / 3, getBodyWidth() / 6, getBodyWidth() / 6);
+		rect[1] = new Rectangle(rightSide - getBodyWidth() / 6, top + getBodyHeight() / 3, getBodyWidth() / 6,
+				getBodyWidth() / 6);
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 2; j++) {
+				g.setColor(dopColor);
+				g.fillRect(rect[j].x, rect[j].y, rect[j].width, rect[j].height);
+				g.setColor(Color.gray.darker());
+				g.drawRect(rect[j].x, rect[j].y, rect[j].width, rect[j].height);
+				if (i < 4) {
+					rect[j].y += getBodyWidth() / 3;
+				}
+			}
+		}
+		g.setColor(Color.BLACK);
+
+		// shit
+		g.drawLine(leftSide + getBodyWidth() / 6, top, leftSide + getBodyWidth() / 6, getBodyHeight());
+		g.drawLine(rightSide - getBodyWidth() / 6, top, getBodyWidth() - getBodyWidth() / 6, getBodyHeight());
+		g.drawRect(leftSide, top + getBodyHeight() / 6, getBodyWidth(), 5);
+		g.drawLine(leftSide, top + getBodyHeight() / 3, leftSide + getBodyWidth() / 6, top + getBodyHeight() / 3);
+		g.drawLine(rightSide, top + getBodyHeight() / 3, rightSide - getBodyWidth() / 6, top + getBodyHeight() / 3);
+		g.drawLine(rect[0].x, rect[0].y + rect[0].height, rect[1].x + rect[1].width, rect[0].y + rect[0].height);
+
+		// фары
+		g.fillRect(leftSide + getBodyWidth() / 6, top + getBodyHeight() / 6 - 5, 8, 5);
+		g.fillRect(rightSide - getBodyWidth() / 6 - 8, top + getBodyHeight() / 6 - 5, 8, 5);
+
+		g.drawRect(body.x, body.y, body.width, body.height);
+	}
+
+	@Override
+	public BufferedImage Pict() {
+		BufferedImage image = new BufferedImage(getBodyWidth() + 1, getBodyHeight() + 1, BufferedImage.TYPE_INT_RGB);
+		Graphics g = image.createGraphics();
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, image.getWidth(), image.getHeight());
+		draw(g);
+		return image;
+	}
+
 }
